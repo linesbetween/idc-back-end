a4363a8ec7f737416ce77bdde2c40983757756e9
zuoli <zli119@stevens.edu>
2020-05-01 15:24:04 -0400

    add upload

---
+++ README.md
@@ -0,0 +1,66 @@
+# Document_Classify
+# A program that reads documents of every kind, guesses what the contents are and creates index of documents.
+
+Run this project:
+run  /src/main/java/com/utpe/dataProcess/DocumentClassifier.java
+training data Set folder      /src/test/data/20news-bydate-train/
+
+### 1. target
+
+An application can read files, guesses what the contents are and creates and index of documents.
+(1) Open a folder, the classifier will classify the documents automatically
+(2) The Classifier could continue to update 
+(3) Store the Classifier and dataset into database
+  
+
+### 2. technology
+
+* Stanford CoreNLP 
+* Protege
+* Java  
+* Java Swing
+* Java Spring Boot
+
+### 3. principle
+⚫ The core part based on the ColumnDataClassifier API of Stanford NLP. It could train data set and predict new files
+
+⚫ Learning and utilizing ontological link distance to figure out the relationship and degree of words, using K means clustering to create its own categories of commonality
+
+* Create folders -> classify documents to different folder
+
+Machine learning part
+
+result:  input pdf,  output label
+need: 1) train dataset
+       has label and pdf
+      2) training algorithm   **
+
+### 4. Components
+(1)Web
+  1) Admin/User send a file to server, return its' category; if the answer is wrong, admin have the authority to send the right answer to the server to learn
+     upload file button
+     appear its' category below
+  2) Upload a fold, return zip of classified documents
+  3) Admin can upload a folder for training 
+  4) User can sign in and sign up
+  sign in/up page
+  
+(2) Desktop
+  Using Java Swing to built UI, it has buttons of "Reset Classifier"  "Open Train Folder to Update", "Open Train Folder for once test", "Open Test Folder",  "Predict File" "Classify Folder"
+    Using "Reset Classifier" could delete the past classifier data
+    Using " Open Train Folder to Update" to update the present Classifier. You should choose a folder stored with files as dataSet, every sub folder's name must be set to the files' category name.
+    Using "Open Train Folder for once test", allow you to train a temporary Classifier
+    Using " Open Test Folder" to test the accuracy of the classifier.
+    Using " Predict File" to predict one file's category
+    Using " Classify Folder" to predict all the files in the folder, and create category folders, place each file to its' according folder automatically
+### 5. The idea of 1th generation product
+(1) 
+(1) set some labels in the database:
+   1) computer science
+   2) physics 
+   3) math
+   4) nutrition
+   5) fiction
+   6) poem
+   7) 
+
--- src/main/java/org/utpe/freeopenuniversity/intelligentdocumentclassifier/controller/MainController.java
+++ src/main/java/org/utpe/freeopenuniversity/intelligentdocumentclassifier/controller/MainController.java
@@ -1,19 +1,30 @@
 package org.utpe.freeopenuniversity.intelligentdocumentclassifier.controller;
 
-//////////////////////   how to remember
+
 import edu.stanford.nlp.classify.ColumnDataClassifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.utpe.freeopenuniversity.intelligentdocumentclassifier.service.dataProcess.ClassifierOperation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.utpe.freeopenuniversity.intelligentdocumentclassifier.service.dataProcess.Constant;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 //接收前端信息， 返回后端数据
 @Controller                              //  set request response
@@ -22,6 +33,7 @@ public class MainController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     @ResponseBody
     public String index() {
+        System.out.println("welcome!");
         return "hello world";
     }
 // 在config产生的bean，在这就直接用
@@ -39,6 +51,38 @@ public class MainController {
         return ClassifierOperation.predictSentence(columnDataClassifier, terms);
 //                documentClassifier.columnDataClassifier.classOf(terms);
     }
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/upload")
+    public String upload() {
+        return "upload";
+    }
+
+    @PostMapping("/upload/{color}")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file,
+                         @PathVariable("color") String color) {
+        if (file.isEmpty()) {
+            return "upload failed";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = Constant.DATAPATH.value + File.separator + "category" + File.separator + color;
+        File dest = new File(filePath + File.separator + fileName);
+        try {
+            new File(filePath).mkdirs();
+            file.transferTo(dest);
+            LOGGER.info("upload successfully in data/category/" + color + " folder");
+            return "upload successfully in data/category/" + color + " folder";
+        } catch (IOException e) {
+            System.out.println(dest);
+            LOGGER.error(e.toString(), e);
+        }
+        return "upload failed！";
+    }
+
 }
 
 // 连接前端静态页面
