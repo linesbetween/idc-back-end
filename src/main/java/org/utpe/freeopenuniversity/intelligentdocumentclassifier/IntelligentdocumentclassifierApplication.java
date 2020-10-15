371cee042229313a6881f7d595d303888788a33d
lines <linesbetween@users.noreply.github.com>
2020-10-15 02:10:49 -0400

    configured CORS

--- src/main/java/org/utpe/freeopenuniversity/intelligentdocumentclassifier/IntelligentdocumentclassifierApplication.java
+++ src/main/java/org/utpe/freeopenuniversity/intelligentdocumentclassifier/IntelligentdocumentclassifierApplication.java
@@ -3,6 +3,9 @@ package org.utpe.freeopenuniversity.intelligentdocumentclassifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.utpe.freeopenuniversity.intelligentdocumentclassifier.service.ui.UI;
 
 @SpringBootApplication
@@ -10,6 +13,16 @@ public class IntelligentdocumentclassifierApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntelligentdocumentclassifierApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
